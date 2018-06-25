786b7f30ed518a64eacd225e841049f88f27a79c
Donvgdong <30788872+Dongvdong@users.noreply.github.com>
2018-06-25 14:59:08 +0800

    Initial commit

---
+++ chapter6/BroadcastBestPractice/app/src/main/java/com/example/broadcastbestpractice/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.example.broadcastbestpractice;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends BaseActivity {
+
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
+
+    private EditText accountEdit;
+
+    private EditText passwordEdit;
+
+    private Button login;
+
+    private CheckBox rememberPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);// 打开登陆布局
+
+        // 读取信息
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+
+        //  按键 编辑框ID
+        accountEdit = (EditText) findViewById(R.id.account);
+        passwordEdit = (EditText) findViewById(R.id.password);
+        rememberPass = (CheckBox) findViewById(R.id.remember_pass);
+        login = (Button) findViewById(R.id.login);
+
+        // 设置 可选按钮 否
+        boolean isRemember = pref.getBoolean("remember_password", false);
+        if (isRemember) {
+            // 将账号和密码 读取到文本框中
+            String account = pref.getString("account", "");
+            String password = pref.getString("password", "");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberPass.setChecked(true);
+        }
+
+        // 登陆  按键函数
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                // 如果账号是admin且密码是123456，就认为登录成功
+                if (account.equals("admin") && password.equals("123456")) {
+                    editor = pref.edit();
+                    if (rememberPass.isChecked()) { // 检查复选框是否被选中
+                        editor.putBoolean("remember_password", true);
+                        editor.putString("account", account);
+                        editor.putString("password", password);
+                    } else {
+                        editor.clear();
+                    }
+                    editor.apply();//  执行储存
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class); //打开下个主界面
+                    startActivity(intent);//开始
+                    finish();//销毁当前
+                } else {
+                    Toast.makeText(LoginActivity.this, "account or password is invalid",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
